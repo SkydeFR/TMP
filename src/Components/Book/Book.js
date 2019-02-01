890aa62d2d4cfdcf442abe52fff809d918e0f120
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-02 00:13:04 +0100

    Légères modifications formulaire réservation

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -7,41 +7,43 @@ class Book extends Component {
     return (
       <div>
         <h1>Réserver</h1>
-        <Form>
-          <FormGroup>
-            <Label for="TypeEvent">Type d'événement</Label>
-            <Input type="select" name="TypeEvent" id="TypeEvent" placeholder="Evénement">
-              <option>Anniversaire</option>
-            </Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="Epoque">Epoque</Label>
-            <Input type="select" name="Epoque" id="Epoque" placeholder="Date">
-              <option>...</option>
-            </Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="Epoque">Destination</Label>
-            <Input type="select" name="Destination" id="Destination" placeholder="Lieu">
-              <option>...</option>
-            </Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="Duree">Durée</Label>
-            <Input type="select" name="Duree" id="Duree" placeholder="Durée">
-              <option>...</option>
-            </Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="DateReservee">Date à réserver</Label>
-            <Input type="date" name="Date" id="Date" placeholder="DateReservee" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="NbPersonnes">Nombre de personne(s)</Label>
-            <Input type="texte" name="NbPersonnes" id="NbPersonnes" placeholder="1" />
-          </FormGroup>
-          <Button>Réserver</Button>
-        </Form>
+        <div class="card">
+          <Form>
+            <FormGroup>
+              <Label for="TypeEvent">Type d'événement</Label>
+              <Input type="select" name="TypeEvent" id="TypeEvent" placeholder="Evénement">
+                <option>Anniversaire</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="Epoque">Epoque</Label>
+              <Input type="select" name="Epoque" id="Epoque" placeholder="Date">
+                <option>...</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="Epoque">Destination</Label>
+              <Input type="select" name="Destination" id="Destination" placeholder="Lieu">
+                <option>...</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="Duree">Durée</Label>
+              <Input type="select" name="Duree" id="Duree" placeholder="Durée">
+                <option>...</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="DateReservee">Date à réserver</Label>
+              <Input type="date" name="Date" id="Date" placeholder="DateReservee" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="NbPersonnes">Nombre de personne(s)</Label>
+              <Input type="texte" name="NbPersonnes" id="NbPersonnes" placeholder="1" />
+            </FormGroup>
+            <Button>Réserver</Button>
+          </Form>
+        </div>
       </div>
     );
   }
