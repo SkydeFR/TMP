3f8059081689bcff790885c67daa879966d45ce8
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-01 21:02:11 +0100

    Opti

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './Components/Home/Home.js';
@@ -19,7 +19,7 @@ class App extends Component {
       <Router basename="/~temoins">
         <div>
           <Navigation />
-          
+
           <Route exact path="/" component={Home} />
           <Route path="/inscription" component={Register} />
           <Route path="/connexion" component={Connect} />
