5cbdddfc55b55ec77b31d4d168be5a6d9f1d5159
Ange Pagel <inkandev@gmail.com>
2019-02-01 15:53:23 +0100

    Conversion en .scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './Components/Home/Home.js';
