90ec969eeb478f423684c185419247739d8969ac
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-01 21:31:23 +0100

    Débuts de formulaire de connexion et d'inscription

--- src/Components/Connect/Connect.js
+++ src/Components/Connect/Connect.js
@@ -4,14 +4,29 @@ import './Connect.css';
 class Connect extends Component {
   render() {
     return (
-        
-      <div>
 
-          <h1>Se connecter</h1>
-          Nom d'utilisateur :
-          Mot de passe :
+        <div>
 
-      </div>
+        <h1>Se connecter</h1>
+        <h2 class="sous-titre">Sous-titre</h2>
+
+        <div class="card">
+            Adresse e-mail
+            <br/>
+            <input type="text" />
+            <br/>
+
+            Mot de passe
+            <br/>
+            <input type="text" />
+
+            <br/>
+            <button>Valider</button>
+            <br/>
+            
+        </div>
+
+        </div>
     );
   }
 }
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -4,9 +4,39 @@ import './Register.css';
 class Register extends Component {
   render() {
     return (
-      <h1>S'inscrire</h1>
 
-      
+        <div>
+
+            <h1>S'inscrire</h1>
+            <h2 class="sous-titre">Sous-titre</h2>
+
+            <div class="card">
+                <br/>
+                Nom
+                <input type="text" />
+
+                Prénom
+                <input type="text" />
+
+                Adresse e-mail
+                <input type="text" />
+               
+                Mot de passe
+                <input type="text" />
+                Confirmer le mot de passe
+                <input type="text" />
+                Date de naissance
+                <input type="text" />
+
+                <br/>
+                <button>Valider</button>
+                <br/>
+                
+            </div>
+
+      </div>
+
+        
     );
   }
 }
