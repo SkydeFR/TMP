e2123a49fb16d92963d0077efd1a44d81a5f5b6b
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-01 23:44:59 +0100

    Ajouts témoignages

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -34,9 +34,19 @@ class Home extends Component {
               <Col>
                 <h2>Les témoignages</h2>
                 <h3>Une satisfaction qui nous tient à coeur</h3>
-                <p>
-                  Bacon ipsum dolor amet leberkas pork loin shankle jerky ham hock turkey ham meatloaf t-bone tongue spare ribs hamburger corned beef ball tip. Meatloaf cupim doner corned beef t-bone pork meatball swine shoulder brisket alcatra chicken turducken tenderloin. Beef spare ribs tail kevin alcatra ball tip short ribs. Jerky strip steak pork prosciutto pork loin hamburger chuck. Shoulder ground round pork belly strip steak, pig andouille hamburger leberkas filet mignon beef kevin brisket burgdoggen. Beef ribs shoulder brisket biltong tail venison salami tenderloin doner.
+                <p>Nous retiendrons un merveilleux voyage, une vue lumineuse, une ambiance chaleureuse, un guide disponible et plein d'humour. Nous repartons les yeux et le coeur remplis de beaux souvenirs.
+                  <br />
+                  <span className="text-danger">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Julie et Kévin, week-end à la Plate-Forme Spaciale "Une" ~ Les derniers instants de la Terre</span>
                 </p>
+                <p>De formidables liens se sont tissés avec les locaux notamment dans les petits villages entourant l'arène de Nephtis, notre fabuleux guide Hien était formidable ! Une expérience hors du commun qui marque et crée de beaux souvenirs.
+                  <br />
+                  <span className="text-danger">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Steven, une semaine sur le satellite SkarlinGrad D8, District "NoSurvival" ~ Combats à morts et visite des villages locaux</span>
+                </p>
+                <p>Voyage extraordinaire et nous pesons nos mots !!! Paysages, faune, flore et rencontres exceptionnelles. Voyage très riche à tous points de vue.
+                  <br />
+                  <span className="text-danger">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Lucie et Niels avec leurs enfants, 2 semaines sur la planète Moohn ~ Sortie culturelle au sein de la forêt Stelia</span>
+                </p>
+
               </Col>
             </Row>
 
