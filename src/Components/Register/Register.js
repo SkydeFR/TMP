fc2dbec3a96d0663d5ebf44eefbfe5ec7ec2f4da
Kévin Kib <sekanor21@gmail.com>
2019-02-02 00:31:46 +0100

    Mise à jour du formulaire d'inscription

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,80 +1,89 @@
 import React, { Component } from 'react';
 import './Register.css';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import * as EmailValidator from 'email-validator';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+ 
 
 
 class Register extends Component {
-  render() {
-    return (
 
-        <div>
-
-            <h1>S'inscrire</h1>
-            <h2 class="sous-titre">Sous-titre</h2>
-
-            <div class="card">
-                <br/>
-                Nom
-                <input type="text" />
-
-                Prénom
-                <input type="text" />
-
-                Adresse e-mail
-                <input type="text" />
-               
-                Mot de passe
-                <input type="text" />
-                Confirmer le mot de passe
-                <input type="text" />
-                Date de naissance
-                <input type="text" />
-
-                <br/>
-                <button>Valider</button>
-                <br/>
-                
-            </div>
-
-      </div>
-
-        
-    );
-  }
-}
-
-class GenderDropdown extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            dropdownOpen: false
+    validateForm() {
+        if (EmailValidator.validate('lol') === true) {
+            console.log('true');
         }
+        else {
+            console.log('false');
+        }
+        
     }
 
-    toggle() {
+    handleChange = event => {
         this.setState({
-            dropdownOpen: !this.state.dropdownOpen
-        })
-    }
+          [event.target.id]: event.target.value
+        });
+    
+        /*
+        if(this.state[event.target.id + "Valide"] === 2) {
+          this.testChange(event);
+        }
+    
+        if(event.target.id === 'password') {
+          if(this.state.cpasswordValide === 1 || this.state.cpasswordValide === 2)
+          {
+            this.setState({cpasswordValide: 0});
+          }
+        }
+        */
+      }
 
     render() {
-        return(
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                <DropdownToggle caret>
-                Button
-                </DropdownToggle>
-                <DropdownMenu>
-                    <DropdownItem>Masculin</DropdownItem>
-                    <DropdownItem>Féminin</DropdownItem>
-                    <DropdownItem>Autre</DropdownItem>
-                </DropdownMenu>
-            </ButtonDropdown>
-        )
-    }
+        return (
 
+            <div>
+
+                <h1>S'inscrire</h1>
+                <h2 class="sous-titre">Sous-titre</h2>
+
+                <div class="card">
+                    <br/>
+
+                    Nom
+                    <input type="text" id="nom" />
+
+                    Prénom
+                    <input type="text" id="prenom" />
+
+                    Adresse e-mail
+                    <input type="text" id="mail" />
+
+                    Genre
+                    <FormGroup>
+                        <Input type="select" name="TypeEvent" id="TypeEvent" placeholder="Evénement">
+                            <option>Masculin</option>
+                            <option>Feminin</option>
+                            <option>Autre</option>
+                        </Input>
+                    </FormGroup>
+                
+                    Mot de passe
+                    <input type="text" id="password" />
+                    Confirmer le mot de passe
+                    <input type="text" id="cpassword" />
+                    Date de naissance
+                    <input type="date" id="datenaissance" />
+
+                    <br/>
+                    <button onClick={this.validateForm}>Valider</button>
+                    <br/>
+                    
+                </div>
+
+            </div>
+
+            
+        );
+    }
 }
 
 export default Register;
