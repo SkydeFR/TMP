6489e1d557adc508b2399da43421b3f290c827b7
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-01 23:23:46 +0100

    Nouvelle description faucon impérial

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,10 +14,10 @@ class Home extends Component {
               <Col>
                 <h2>Le Faucon Impérial</h2>
                 <h3>Au coeur de votre voyage</h3>
-                <p>Le Faucon Impérial n'est pas à proprement parler une « machine » à voyager dans le temps : il contient une grande quantité de composants mécaniques et électroniques, mais une partie est vivante.<br />
-                  Il s'agit donc d'une entité nécessitant le plus grand soin, dont nos spécialistes font part.<br />
-                  A l'aide de notre Faucon Impérial, il vous sera possible de voyager en toute sécurité dans les autres univers à n'importe une finité d'époques.<br />
-                  Les deux seules limites étant l'impossibilité de franchir le « Voïd », ce qui l'empêche de passer dans des mondes parallèles.<br />
+                <p>Le Faucon Impérial n'est pas à proprement parler une « machine » à voyager dans le temps : il s'agit donc d'une entité vivante.
+                  A l'aide de notre Faucon Impérial, il vous sera possible de voyager en toute sécurité dans une infinité d'autres univers à n'importe quelle époque.
+                  <br /><br />
+                  Les deux seules limites étant l'impossibilité de franchir le « Voïd », ce qui l'empêche de passer dans des mondes parallèles.
                   Il ne peut également pas voyager à une date antérieure au début de l'univers, ni postérieure à sa fin.</p>
               </Col>
               <Col>
