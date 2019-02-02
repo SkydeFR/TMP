e0c9fa580a5fb360734c1c3450785db707cf7198
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-02 08:33:31 +0100

    Retrait page de réservation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home/Home.js';
 import Register from './Components/Register/Register.js';
 import Connect from './Components/Connect/Connect.js';
-import Book from './Components/Book/Book.js';
 import Trips from './Components/Trips/Trips.js';
 import Navigation from './Components/StructurePage/Navigation.js';
 import Footer from './Components/StructurePage/Footer.js';
@@ -23,7 +22,6 @@ class App extends Component {
           <Navigation />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/reserver" component={Book} />
             <Route path="/destinations" component={Trips} />
             <Route path="/connexion" component={Connect} />
             <Route path="/inscription" component={Register} />
