b4a3e5653a33c5ff09c51b099c06b73de4f938fe
Kévin Kib <sekanor21@gmail.com>
2019-02-02 02:07:31 +0100

    Correction du message de page non trouvée

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -5,7 +5,7 @@ class NotFound extends Component {
     render() {
         return (
             <div>
-                <h1>404 - Page non trouvé</h1>
+                <h1>404 - Page non trouvée.</h1>
             </div>
         );
     }
