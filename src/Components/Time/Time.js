888a32cc2e134fc20f7bf09acb7dff9dcb16bc77
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-02 09:14:17 +0100

    Modif texte

--- src/Components/Time/Time.js
+++ src/Components/Time/Time.js
@@ -109,7 +109,7 @@ class Time extends Component {
                 <h3>À travers les époques. Littéralement.</h3>
                 <p>
                   Le Faucon Impérial n'est pas à proprement parler une « machine » à voyager dans le temps : il s'agit donc d'une entité vivante.
-                    A l'aide de notre Faucon Impérial, il vous sera possible de voyager en toute sécurité dans une infinité d'autres univers à n'importe quelle époque.
+                    A l'aide de notre Faucon Impérial, il vous sera possible de voyager en toute sécurité dans une infinité d'époques différentes.
                   <br /><br />
                   Les deux seules limites étant l'impossibilité de franchir le « Voïd », ce qui l'empêche de passer dans des mondes parallèles.
                   Il ne peut également pas voyager à une date antérieure au début de l'univers, ni postérieure à sa fin.
