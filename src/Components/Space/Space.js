06284bb191107d2b9c8c6460ed89142678094517
Ange Pagel <inkandev@gmail.com>
2019-02-02 03:47:54 +0100

    Mise à jour des pages Temps et Espace

--- src/Components/Space/Space.js
+++ src/Components/Space/Space.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './Space.css';
 
 class Space extends Component {
   render() {
     return (
       <div>
-        <h1>Espace</h1>
+        <h1>L'Espace</h1>
+        <h2 class="sous-titre">Croisières sur-mesure</h2>
+        <section id="espace-presentation" class="dark-theme">
+          <Container>
+            <Row>
+              <Col>
+                <h2>Le Mega Stardust 5210</h2>
+                <h3>Le Système Solaire à vol d'oiseau</h3>
+                <p>
+                  Lorem ipsum
+                </p>
+              </Col>
+              <Col>
+
+              </Col>
+            </Row>
+          </Container>
+        </section>
       </div>
     );
+
   }
 }
 
--- src/Components/Time/Time.js
+++ src/Components/Time/Time.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './Time.css';
 
 class Time extends Component {
   render() {
     return (
       <div>
-        <h1>Temps</h1>
+        <h1>Le Temps</h1>
+        <h2 class="sous-titre">Revivez l'Histoire, vivez la vôtre</h2>
+        <section id="temps-presentation" class="dark-theme">
+          <Container>
+            <Row>
+              <Col>
+                <h2>Le Faucon Impérial</h2>
+                <h3>À travers les époques. Littéralement.</h3>
+                <p>
+                  Lorem ipsum
+                </p>
+              </Col>
+              <Col>
+
+              </Col>
+            </Row>
+          </Container>
+        </section>
       </div>
     );
   }
